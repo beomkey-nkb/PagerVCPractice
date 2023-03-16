34cea835606dc637de6d1578104468952ea595cf
BeomKey <nkb5813@naver.com>
2023-03-16 11:09:12 +0900

    기존 레이아웃 compositional layout으로 교체

--- PagerVCPractice/PagerVCPractice/View/MainPager/RandomImageList/DayWebtoonListViewController.swift
+++ PagerVCPractice/PagerVCPractice/View/MainPager/RandomImageList/DayWebtoonListViewController.swift
@@ -9,16 +9,14 @@ import Foundation
 import UIKit
 import Combine
 
+enum DayWebtoonListSection: Int {
+    case dayWebtoonList
+}
+
 final class DayWebtoonListViewController: UIViewController {
     private var viewModel = DayWebtoonListViewModel()
-    private lazy var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.minimumLineSpacing = 10
-        layout.scrollDirection = .vertical
-        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        return UICollectionView(frame: .zero, collectionViewLayout: layout)
-    }()
     
+    private var collectionView: UICollectionView! = nil
     private var dataSource: UICollectionViewDiffableDataSource<Section, WebtoonImageCellViewModel>!
     private var cancellables = Set<AnyCancellable>()
     
@@ -29,6 +27,7 @@ final class DayWebtoonListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupCollectionView()
         setupLayout()
         setupStyling()
         setupCollectionViewDataSource()
@@ -71,16 +70,11 @@ extension DayWebtoonListViewController {
     }
     
     func setupStyling() {
-        collectionView.backgroundColor = .clear
-        collectionView.showsHorizontalScrollIndicator = false
-        collectionView.isScrollEnabled = true
-        collectionView.showsVerticalScrollIndicator = false
-        collectionView.delegate = self
-        collectionView.registerCell(cellType: DayWebtoonCollectionViewCell.self)
+        view.backgroundColor = .white
     }
 }
 
-// MARK: CollectionView setup
+// MARK: CollectionView dataSource
 
 extension DayWebtoonListViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
@@ -101,12 +95,70 @@ extension DayWebtoonListViewController: UICollectionViewDelegate, UICollectionVi
         snapshot.appendItems(cellViewModels, toSection: .landomImageList)
         dataSource.apply(snapshot, animatingDifferences: false)
     }
+}
+
+// MARK: CollectionView layout
+
+private extension DayWebtoonListViewController {
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let screenWidth = UIScreen.main.bounds.width
-        let paddingX = 32
-        let spacing = 16
-        let cellWidth = (Int(screenWidth) - paddingX - spacing) / 3 - 2
-        return CGSize(width: cellWidth, height: 200)
+    func setupCollectionView() {
+        collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: createCollectionViewCompositionalLayout()
+        )
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isScrollEnabled = true
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.delegate = self
+        collectionView.registerCell(cellType: DayWebtoonCollectionViewCell.self)
+    }
+    
+    func createCollectionViewCompositionalLayout() -> UICollectionViewLayout {
+        let config = UICollectionViewCompositionalLayoutConfiguration()
+        config.interSectionSpacing = 0
+        
+        let layout = UICollectionViewCompositionalLayout { [weak self] sectionIndex, layoutEnvironment in
+            guard let section = DayWebtoonListSection(rawValue: sectionIndex) else { return nil }
+            return self?.createCollectionLayout(with: section)
+        }
+        
+        layout.configuration = config
+        return layout
+    }
+    
+    func createCollectionLayout(with section: DayWebtoonListSection) -> NSCollectionLayoutSection {
+        switch section {
+        case .dayWebtoonList:
+            return createDayWebtoonListLayout()
+        }
+    }
+    
+    func createDayWebtoonListLayout() -> NSCollectionLayoutSection {
+        let webtoonContainerSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.3),
+            heightDimension: .absolute(200)
+        )
+        
+        let webtoonItem = NSCollectionLayoutItem(layoutSize: webtoonContainerSize)
+        webtoonItem.contentInsets = .spacing(5)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(200)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitem: webtoonItem,
+            count: 3
+        )
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+}
+
+private extension NSDirectionalEdgeInsets {
+    static func spacing(_ value: CGFloat) -> NSDirectionalEdgeInsets {
+        return NSDirectionalEdgeInsets(top: value, leading: value, bottom: value, trailing: value)
     }
 }
