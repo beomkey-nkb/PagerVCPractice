6dfe8047d4f80610844e5ed7625f0ba4d4ec40ad
BeomKey <nkb5813@naver.com>
2023-05-03 22:07:21 +0900

    Tabbar 구현

--- PagerVCPractice/PagerVCPractice/App/AppDelegate.swift
+++ PagerVCPractice/PagerVCPractice/App/AppDelegate.swift
@@ -14,7 +14,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         let nav = UINavigationController(rootViewController: MainPagerViewController())
-        window?.rootViewController = nav
+        nav.tabBarItem.tag = 0
+        let tabbarController = UITabBarController()
+        tabbarController.tabBar.tintColor = .black
+        if #available(iOS 15.0, *) {
+            tabbarController.tabBar.scrollEdgeAppearance = tabbarController.tabBar.standardAppearance
+        }
+        
+        tabbarController.viewControllers = [nav]
+        tabbarController.tabBar.items?.forEach({ item in
+            item.title = "main"
+            item.image = UIImage(named: "tabbarMain")
+        })
+        window?.rootViewController = tabbarController
         window?.makeKeyAndVisible()
         return true
     }
